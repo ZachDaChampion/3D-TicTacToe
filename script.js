aea7263be7a4ef9dad6c35f0970efdff0bf0ec33
ZachDaChampion <zachchampion79@gmail.com>
2020-01-14 09:11:58 -0500

    add third player

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ class GridItem {
 
         this.owner = o;
         if (this.owner == 1) this.reference.style.backgroundColor = 'red';
-        if (this.owner == -1) this.reference.style.backgroundColor = 'blue';
+        if (this.owner == 2) this.reference.style.backgroundColor = 'blue';
+        if (this.owner == 3) this.reference.style.backgroundColor = 'green';
         if (this.owner == 0) this.reference.style.backgroundColor = 'transparent';
     }
 }
@@ -65,7 +66,7 @@ class GridLookup {
 function mouse_over(square) {
 
     let lookup = GridLookup.lookup_id(square.id);
-    if (lookup.get().owner == 0) lookup.get().reference.style.borderColor = (turn % 2 == 0) ? 'red' : 'blue';
+    if (lookup.get().owner == 0) lookup.get().reference.style.borderColor = (turn % 3 == 0) ? 'red' : (turn % 3 == 1 ? 'blue' : 'green');
 }
 
 function mouse_leave(square) {
@@ -78,17 +79,33 @@ function mouse_click(square) {
 
     let lookup = GridLookup.lookup_id(square.id);
     if (lookup.get().owner == 0) {
-        lookup.get().set_owner(turn % 2 == 0 ? 1 : -1);
+        lookup.get().set_owner(turn % 3 == 0 ? 1 : (turn % 3 == 1 ? 2 : 3));
         turn++;
     }
 
     let red_victory = check_win(1, grid);
-    let blue_victory = check_win(-1, grid);
+    let blue_victory = check_win(2, grid);
+    let green_victory = check_win(3, grid);
 
     setTimeout(function() {
 
-        if (red_victory && blue_victory) {
-            window.alert("Tie");
+        if (red_victory && blue_victory && green_victory) {
+            window.alert("3 Way Tie");
+            window.location.reload(false);
+        }
+
+        else if (red_victory && blue_victory) {
+            window.alert("2 way tie between red and blue!");
+            window.location.reload(false);
+        }
+
+        else if (red_victory && green_victory) {
+            window.alert("2 way tie between red and green!");
+            window.location.reload(false);
+        }
+
+        else if (blue_victory && green_victory) {
+            window.alert("2 way tie between blue and green!");
             window.location.reload(false);
         }
 
@@ -98,6 +115,10 @@ function mouse_click(square) {
         }
         else if (blue_victory) {
             window.alert("Blue wins!");
+            window.location.reload(false);
+        }
+        else if (green_victory) {
+            window.alert("Green wins!");
             window.location.reload(false);
         }
     }, 100);
